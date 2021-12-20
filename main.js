b203c64abf57257210664811e8841f5adb586b56
manvi21 <manvibohra321@gmail.com>
2021-12-20 20:31:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+    var player1 = "Player 1";
+    var player2 = "Player 2";
+
+    
+
+    function rollTheDice() {
+      
+            var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+            var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+            document.querySelector(".img1").setAttribute("src",
+                "dice" + randomNumber1 + ".png");
+
+            document.querySelector(".img2").setAttribute("src",
+                "dice" + randomNumber2 + ".png");
+
+            if (randomNumber1 === randomNumber2) {
+                document.querySelector("h1").innerHTML = "Draw!";
+            }
+
+            else if (randomNumber1 < randomNumber2) {
+                document.querySelector("h1").innerHTML
+                    = (player2 + " WINS!");
+            }
+
+            else {
+                document.querySelector("h1").innerHTML
+                    = (player1 + " WINS!");
+            }
+    
+    }
